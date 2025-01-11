ee5d6653a16accb52a0aad70c7323ead11ef48c5
Pranav Nair <nairpranav31@gmail.com>
2025-01-11 12:34:09 +0530

    d

--- Exp/src/Main/Main.java
+++ Exp/src/Main/Main.java
@@ -1,47 +1,7 @@
 package Main;
 
-abstract class Policy{
-	
-	String name = "xzr";
-	void displayName() {
-		System.out.println("Name "+name);
-	}
-	void getLeaves() {
-		System.out.println("Leave approved");
-	}
-	abstract double calSal(double sal) ;
-}
-
-class Acc extends Policy {
-	@Override
-	double calSal(double sal) {
-		return sal;
-	}
-}
-
-class Man extends Policy {
-	@Override
-	double calSal(double sal) {
-		return sal;
-	}
-}
-
-class Emp extends Policy {
-	@Override
-	double calSal(double sal) {
-		return sal;
-	}
-}
-
 public class Main {
 	public static void main(String[] args) {
-		Acc acc = new Acc();
-		System.out.println(acc.calSal(40));
-		acc.getLeaves();
-		Man man = new Man();
-		System.out.println(man.calSal(50));
-		Emp emp = new Emp();
-		System.out.println(emp.calSal(60));
-		emp.displayName();
+		System.out.println("Hello Worlds");
 	}
 }
